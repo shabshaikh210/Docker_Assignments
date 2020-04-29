39ecd5672dca13e7d27882cf200e1159f4c35b17
Shabana R. Shaikh <53016285+shabshaikh210@users.noreply.github.com>
2020-04-29 20:16:22 +0530

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,8 @@
+create table Student(ID int,Name varchar(30),Age int,Department varchar(30),Subject Varchar(30));
+insert into Student values(1,'Alex',22,'Comp_Sci','CN,DS,OS');
+insert into Student values(3,'John',21,'Electronics','Digital, Analog');
+insert into Student values(2,'Katnees',23,'Mathematics','NM,Applied');
+insert into Student values(4,'Chris',22,'Comp_Sci','CN,DS,OS');
+insert into Student values(5,'Gale',22,'Electronics','Digital, Analog');
+insert into Student values(6,'Prim',23,'Mathematics','NM,Applied');
+
